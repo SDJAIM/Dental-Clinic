ee84a8bb5f8e91489c3f88c857857a3049218e74
SDJAIM <susandejesus96@gmail.com>
2025-05-29 15:06:47 +0200

    Delete Wizard

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from . import models
 from . import controllers
-from . import wizard  # Si usas asistentes (wizards), descomenta esta línea y asegúrate de tener el directorio con __init__.py
